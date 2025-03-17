d672f477fe9c1129996d8363cf1e624d32e524e0
Rick <stevense00@gmail.com>
2025-03-16 21:48:48 -0700

    Adding callback stop to model_2

--- model_2_1.py
+++ model_2_1.py
@@ -1,4 +1,14 @@
 import tensorflow as tf
+
+target_accuracy = 0.90
+
+class trainCB(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy')>target_accuracy):
+            print("\nReached " + str(target_accuracy*100) + "% accuracy, training stopped")
+            self.model.stop_training = True
+
+callbacks = trainCB()
 data = tf.keras.datasets.fashion_mnist
 
 (training_images, training_labels),(test_images, test_labels) = data.load_data()
@@ -6,14 +16,18 @@ data = tf.keras.datasets.fashion_mnist
 training_images = training_images / 255.0
 test_images = test_images / 255.0
 
+training_images = training_images
+training_labels = training_labels
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28,28)),
     tf.keras.layers.Dense(128, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-    ])
+])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=100, callbacks=[callbacks])
+model.evaluate(test_images, test_labels)
